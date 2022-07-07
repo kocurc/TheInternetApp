43053b5c26ccde70cc38370a874b68d5b4422723
Kordian Ojrzyński <kordian.o@wp.pl>
2022-07-07 23:31:02 +0200

    Add nigtly yaml.

--- TheInternetApp/PageObjects/SortableDataTables/SortableDataTablesObject.cs
+++ TheInternetApp/PageObjects/SortableDataTables/SortableDataTablesObject.cs
@@ -1,6 +1,17 @@
-﻿namespace TheInternetApp.PageObjects.SortableDataTables
+﻿using OpenQA.Selenium;
+using TheInternetApp.PageObjects.Base;
+using TheInternetApp.PageObjects.SortableDataTables.Interfaces;
+
+namespace TheInternetApp.PageObjects.SortableDataTables;
+
+public class HorizontalSliderObject : BasePageObject
 {
-    public class HorizontalSliderObject
+    public IDataTable Example1DataTable { get; init; }
+    public IDataTable Example2DataTable { get; init; }
+
+    public HorizontalSliderObject(IWebDriver webDriver) : base(webDriver)
     {
+        Example1DataTable = new DataTable.DataTable("table1", WebDriver);
+        Example2DataTable = new DataTable.DataTable("table2", WebDriver);
     }
 }
